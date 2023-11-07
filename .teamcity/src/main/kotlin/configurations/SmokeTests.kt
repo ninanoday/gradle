63537595ece1db948ba584ef69973115187747a1
Bo Zhang <bo@gradle.com>
2023-11-07 16:42:18 +0800

    Remove specific Java version from docsTest and smokeTest
    Previously we had Java version in docsTest/smokeTest, like
    `Gradle_Master_Check_DocsTest_Java21_Linux_1`.
    
    This is unnecessary and actually causes some problem:
    we can't test it on feature branches. Let's remove
    the version from build type id.

--- .teamcity/src/main/kotlin/configurations/SmokeTests.kt
+++ .teamcity/src/main/kotlin/configurations/SmokeTests.kt
@@ -8,7 +8,7 @@ import model.CIBuildModel
 import model.Stage
 
 class SmokeTests(model: CIBuildModel, stage: Stage, testJava: JvmCategory, task: String = "smokeTest", splitNumber: Int = 1) : BaseGradleBuildType(stage = stage, init = {
-    id("${model.projectId}_${task.toCapitalized()}s${testJava.version.name.toCapitalized()}")
+    id("${model.projectId}_${task.toCapitalized()}s")
     name = "Smoke Tests with 3rd Party Plugins ($task) - ${testJava.version.name.toCapitalized()} Linux"
     description = "Smoke tests against third party plugins to see if they still work with the current Gradle version"
 
